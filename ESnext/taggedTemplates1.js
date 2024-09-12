ca33aa72f85fc96e7ecacd7dfbff14b90e024438
Kevyn Lima <kevynlimasilva1@gmail.com>
2024-09-12 17:52:46 -0300

    Exemplos de tagged templates

--- ESnext/taggedTemplates1.js
+++ ESnext/taggedTemplates1.js
@@ -1,1 +1,25 @@
 //tagged templates - A ideia é que consiga processar um template string dentro de uma função.
+function tag(partes,...valores){
+    console.log(partes)
+    console.log(valores)
+    return 'Outra string'
+}
+
+const aluno = 'Gui'
+const situacao = 'Aprovado'
+
+console.log(tag `${aluno} está ${situacao}`)
+
+/** Um segundo exemplo tagged template */
+
+function real(partes, ...valores){
+    resultado = []
+    valores.forEach((valor, indice) => {
+        valor = isNaN(valor) ? valor : `R$${valor.toFixed(2)}`
+        resultado.push(partes[indice], valor)
+    })
+    return resultado.join('')
+}
+const preco = 29.99
+const precoParcela = 11
+console.log(real `1x de ${preco} ou 3x de ${precoParcela}`)
